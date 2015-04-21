dc71017e38d2765677ce0c6933bb59e7ac5de0bf
rdamm <ry.damm@gmail.com>
2015-04-21 12:23:22 -0600

    Cleaned Up the Code
    Cleaned up the code a bit for submission.

--- Model/ChessRatingAnalysis.R
+++ Model/ChessRatingAnalysis.R
@@ -21,96 +21,52 @@ hdfs.init()     # Initialize hadoop
 
 ##### Initialization.END
 
-##### Data Initialization
+# Define the location of the input files in the HDFS.
+gamedata_uci <- "/user/chess/traindata_uci.pgn";
+engine_score <- "/user/chess/stockfish.csv";
 
-# Directories
 
-# Two possible chess move formats. UCI seems easier for analysis,
-# SAN is easier for display. These should be in your hadoop directory.
-gamedata <- "/user/chess/data.pgn"
-gamedata_uci <- "/user/chess/traindata_uci.pgn"
-engine_score <- "/user/chess/stockfish.csv"
+### Read in the game metadata.
 
-# Definition of Data Format
+# Define the game file format.
+gamedata_uci.classes <- c(
+  GameNumber = "character", Source = "factor", Date = "factor", Round = "factor", WhiteName = "factor", BlackName = "factor",
+  Result = "factor", WhiteElo = "character", BlackElo = "character", Moves = "character"
+);
+gamedata_uci.format <- make.input.format(
+  "csv",
+  sep = ",",
+  colClasses = gamedata_uci.classes,
+  col.names = names(gamedata_uci.classes)
+);
 
-gamedata_uci.classes <-
-  c(
-    GameNumber  = "character",  Source    = "factor",	
-    Date  = "factor",			Round       = "factor",	
-    WhiteName = "factor", 		BlackName = "factor",
-    Result      = "factor",		WhiteElo  = "character",  	
-    BlackElo 	= "character",	Moves = "character"
-  )
-
-# Custom format for MapReduce tasks
-
-gamedata_uci.format <-
-  make.input.format(
-    "csv",
-    sep = ",",
-    colClasses = gamedata_uci.classes,
-    col.names = names(gamedata_uci.classes)
-  )
-
-##### Data Initialization.END
-
-##### Data Processing
-
-## Read Data 
-
-#TODO Decided between mapreduce and from.dfs reading method. Or take this out.
-
-# Start MapReduce function to read
+# Read the game metadata.
 mapreduce.chess <- mapreduce(
-  input = gamedata_uci,       	    # Input File
-  input.format = gamedata_uci.format	# Input Format
-)
+  input = gamedata_uci,
+  input.format = gamedata_uci.format
+);
+mapreduce.chess.values <- values(from.dfs(mapreduce.chess));
+View(mapreduce.chess.values);
 
-# # Store values from MapReduce output
-mapreduce.chess.values <- values(from.dfs(mapreduce.chess))
-
-# # View Data
-View(mapreduce.chess.values)
-
-## Read Data.END
-
-## Filter Data
-
-#TODO 	Make filter remove extraneous characters (result, Square brackets,
-#		quotes...)
-map.chess.filter<-function(.,lines){
-  value <- cbind( lines$GameNumber, lines$WhiteElo, lines$BlackElo, lines$Moves)
-  keyval(lines$GameNumber, value)
-}
-
+# Filter out the extraneous characters (result, square brackets, quotes, etc.).
+map.chess.filter <- function(.,lines) {
+  value <- cbind(lines$GameNumber, lines$WhiteElo, lines$BlackElo, lines$Moves);
+  keyval(lines$GameNumber, value);
+};
 reduce.chess.filter <- function(key,values)
 {
-  keyval(key, values)
+  keyval(key, values);
 }
-
-# Start MapReduce function
 mapreduce.chess.filter <- mapreduce(
-  input = gamedata_uci,       	    # Input File
-  input.format = gamedata_uci.format,	# Input Format
-  map = map.chess.filter#,             # Map Function
-  #reduce = reduce.chess.filter 		# Reduce Function
+  input = gamedata_uci,
+  input.format = gamedata_uci.format,
+  map = map.chess.filter
 )
-
-# Store keys and values from MapReduce output
-mapreduce.chess.filter.values <- values(from.dfs(mapreduce.chess.filter))
-mapreduce.chess.filter.keys <-  keys(from.dfs(mapreduce.chess.filter))
-
-# View Data
-View(mapreduce.chess.filter.values)
-
-## Filter Data.END
-
-##### Data Processing.END
+mapreduce.chess.filter.values <- values(from.dfs(mapreduce.chess.filter));
+View(mapreduce.chess.filter.values);
 
 
-########################################
 ### Read in the scores for the games.
-########################################
 
 # Define the score file format.
 scoreData_classes <- c(GameNumber = "character", Score = "character");
@@ -122,28 +78,20 @@ scoreData_format <- make.input.format(
 );
 
 # Define the delta average mapreduce function.
-# TODO: Ignore the first line (headers).
 findDeltaAverages = function(input)
 {
   # Define the mapper.
   findDeltaAverages_map = function(., lines)
   {
-    # Get the line inputs.
-    gameNumber <- as.character(lines$GameNumber);
-    scoreList <- as.character(lines$Score);
-    
-    return(keyval(gameNumber, scoreList));
+    return(keyval(as.character(lines$GameNumber), as.character(lines$Score)));
   }
   
   # Define the reducer.
   findDeltaAverages_reduce = function(key, values)
   {
-    # Key = GameNumber, Values = ScoreList
-    gameNumber <- as.character(key);
-    scoreList <- as.character(values);
-    
     # Split the scores up into a vector.
     scoresVector <- c();
+    scoreList <- as.character(values);
     if (is.na(scoreList) == FALSE)
     {
       scoresVector <- unlist(strsplit(scoreList, " "));
@@ -166,35 +114,25 @@ findDeltaAverages = function(input)
       # Negate the black deltas since they go in the opposite direction.
       blackDeltas <- -blackDeltas;
       
-      # Get the mean and standard deviation for each player.
+      # Get the mean, standard deviation, and mean of the lowest 10 scores for each player.
       meanWhiteDelta <- mean(whiteDeltas);
       meanBlackDelta <- mean(blackDeltas);
       stdDevWhiteDelta <- sd(whiteDeltas);
       stdDevBlackDelta <- sd(blackDeltas);
+      meanLowestWhiteDeltas <- mean(whiteDeltas[which(whiteDeltas %in% head(sort(whiteDeltas), 10))]);
+      meanLowestBlackDeltas <- mean(blackDeltas[which(blackDeltas %in% head(sort(blackDeltas), 10))]);
       
-      # Get the average of each player's lowest 10 scores.
-      lowestWhiteDeltas <- whiteDeltas[which(whiteDeltas %in% head(sort(whiteDeltas), 10))];
-      lowestBlackDeltas <- blackDeltas[which(blackDeltas %in% head(sort(blackDeltas), 10))];
-      meanLowestWhiteDeltas <- mean(lowestWhiteDeltas);
-      meanLowestBlackDeltas <- mean(lowestBlackDeltas);
-      
-      # Encapsulate the values in a data frame.
-      retVals <- data.frame(WhiteAverageDeltaScore = meanWhiteDelta, BlackAverageDeltaScore = meanBlackDelta,
-                            WhiteStdDevDeltaScore = stdDevWhiteDelta, BlackStdDevDeltaScore = stdDevBlackDelta,
-                            WhiteLowestDeltaScore = meanLowestWhiteDeltas, BlackLowestDeltaScore = meanLowestBlackDeltas);
-      
-      #return(keyval(gameNumber, averageWhiteDelta));
-      return(keyval(gameNumber, retVals));
+      # Return all the score data for this game.
+      return(keyval(as.character(key), data.frame(WhiteAverageDeltaScore = meanWhiteDelta, BlackAverageDeltaScore = meanBlackDelta,
+                                           WhiteStdDevDeltaScore = stdDevWhiteDelta, BlackStdDevDeltaScore = stdDevBlackDelta,
+                                           WhiteLowestDeltaScore = meanLowestWhiteDeltas, BlackLowestDeltaScore = meanLowestBlackDeltas)));
     }
     else
     {
-      # Encapsulate the values in a data frame.
-      retVals <- data.frame(WhiteAverageDeltaScore = NA, BlackAverageDeltaScore = NA,
-                            WhiteStdDevDeltaScore = NA, BlackStdDevDeltaScore = NA,
-                            WhiteLowestDeltaScore = NA, BlackLowestDeltaScore = NA);
-      
-      # TODO: Use NA as default value return.
-      return(keyval(gameNumber, retVals));
+      # Return NA for all values since this line is not a valid game.
+      return(keyval(as.character(key), data.frame(WhiteAverageDeltaScore = NA, BlackAverageDeltaScore = NA,
+                                           WhiteStdDevDeltaScore = NA, BlackStdDevDeltaScore = NA,
+                                           WhiteLowestDeltaScore = NA, BlackLowestDeltaScore = NA)));
     }
   }
   
@@ -211,34 +149,26 @@ findDeltaAverages = function(input)
 results <- findDeltaAverages(engine_score);
 results_values <- values(from.dfs(results));
 
-# TODO: Limit the file read to only the first 24999 entries.
+# Limit the score data to only the first 24999 entries to match the training data.
 results_values <- results_values[1:24999,];
 
 
-########################################
 ### Prep the data for model generation.
-########################################
 
 # Convert the values from a matrix into a data frame.
 mapreduce.chess.filter.values <- data.frame(mapreduce.chess.filter.values);
-
-# Get the number of rows from the data set.
-numRows <- nrow(mapreduce.chess.filter.values);
-
-# Set the column names.
 colnames(mapreduce.chess.filter.values) <- c("EventId", "WhiteElo", "BlackElo", "MoveList");
 
 # Extract the number values from the ELO strings.  This requires creating a temporary data frame
 # where the columns are integers so that the conversion works ok.
 tempArray <- data.frame(WhiteElo = numeric(0), BlackElo = numeric(0));
-for (i in 1:numRows)
+for (i in 1:nrow(mapreduce.chess.filter.values))
 {
   tempArray[i, "WhiteElo"] <- gsub("[^0-9]", "", sapply(mapreduce.chess.filter.values[i, "WhiteElo"], as.character));
   tempArray[i, "BlackElo"] <- gsub("[^0-9]", "", sapply(mapreduce.chess.filter.values[i, "BlackElo"], as.character));
 }
 mapreduce.chess.filter.values$WhiteElo = tempArray[,"WhiteElo"];
 mapreduce.chess.filter.values$BlackElo = tempArray[,"BlackElo"];
-#mapreduce.chess.filter.values <- transform(mapreduce.chess.filter.values, WhiteElo = as.numeric(WhiteElo), BlackElo = as.numeric(BlackElo));
 
 # Add the average delta scores of each game to the data set.
 mapreduce.chess.filter.values$WhiteAverageDeltaScore <- results_values$WhiteAverageDeltaScore;
@@ -249,26 +179,24 @@ mapreduce.chess.filter.values$WhiteLowestMeanDeltaScore <- results_values$WhiteL
 mapreduce.chess.filter.values$BlackLowestMeanDeltaScore <- results_values$BlackLowestDeltaScore;
 
 # Copy all delta scores and ELOs into a new data frame where white and black do not factor in so we can make the model.
-allElos <- as.numeric(c(mapreduce.chess.filter.values[,"WhiteElo"], mapreduce.chess.filter.values[,"BlackElo"]));
-allAverageDeltaScores <- as.numeric(c(mapreduce.chess.filter.values[,"WhiteAverageDeltaScore"], mapreduce.chess.filter.values[,"BlackAverageDeltaScore"]));
-allStdDevDeltaScores <- as.numeric(c(mapreduce.chess.filter.values[,"WhiteStdDevDeltaScore"], mapreduce.chess.filter.values[,"BlackStdDevDeltaScore"]));
-allLowestMeanDeltaScores <- as.numeric(c(mapreduce.chess.filter.values[,"WhiteLowestMeanDeltaScore"], mapreduce.chess.filter.values[,"BlackLowestMeanDeltaScore"]));
-scoresToElos <- data.frame(AverageDeltaScore = allAverageDeltaScores, StdDevDeltaScore = allStdDevDeltaScores, LowestMeanDeltaScore = allLowestMeanDeltaScores, Elo = allElos);
+scoresToElos <- data.frame(
+  AverageDeltaScore = as.numeric(c(mapreduce.chess.filter.values[,"WhiteAverageDeltaScore"], mapreduce.chess.filter.values[,"BlackAverageDeltaScore"])),
+  StdDevDeltaScore = as.numeric(c(mapreduce.chess.filter.values[,"WhiteStdDevDeltaScore"], mapreduce.chess.filter.values[,"BlackStdDevDeltaScore"])),
+  LowestMeanDeltaScore = as.numeric(c(mapreduce.chess.filter.values[,"WhiteLowestMeanDeltaScore"], mapreduce.chess.filter.values[,"BlackLowestMeanDeltaScore"])),
+  Elo = as.numeric(c(mapreduce.chess.filter.values[,"WhiteElo"], mapreduce.chess.filter.values[,"BlackElo"]))
+);
 
 # Remove any rows containing NA.
 scoresToElos <- na.omit(scoresToElos);
 
 
-########################################
 ### Generate the models for the data.
-########################################
 
 # Summarize the data.
 summary(scoresToElos);
 cor(scoresToElos);
 
 # Plot the variables against each other.
-# NOTE: This takes a long time to plot.
 #pairs(scoresToElos);
 
 # Create linear regression models for mean delta score to Elo, std dev delta score to Elo.
@@ -278,14 +206,10 @@ lowestMeanModel <- lm(Elo ~ LowestMeanDeltaScore, data = scoresToElos);
 multiModel <- lm(Elo ~ AverageDeltaScore * StdDevDeltaScore * LowestMeanDeltaScore, data = scoresToElos);
 
 # Get the linear models in the form Y = aX + b.
-meanModelCoefficients = coefficients(meanModel);
-stdDevModelCoefficients = coefficients(stdDevModel);
-lowestMeanModelCoefficients = coefficients(lowestMeanModel);
-multiCoefficients = coefficients(multiModel);
-print(meanModelCoefficients);
-print(stdDevModelCoefficients);
-print(lowestMeanModelCoefficients);
-print(multiCoefficients);
+print(coefficients(meanModel));
+print(coefficients(stdDevModel));
+print(coefficients(lowestMeanModel));
+print(coefficients(multiModel));
 
 # Get the summaries of the models.
 summary(meanModel);
@@ -298,19 +222,13 @@ anova(lowestMeanModel);
 anova(multiModel);
 
 # Compare the fitted values and residuals for each model.
-meanModelValuesAndResiduals <- data.frame(FittedValue = fitted.values(meanModel), Residual = residuals(meanModel));
-stdDevModelValuesAndResiduals <- data.frame(FittedValue = fitted.values(stdDevModel), Residual = residuals(stdDevModel));
-lowestMeanModelValuesAndResiduals <- data.frame(FittedValue = fitted.values(lowestMeanModel), Residual = residuals(lowestMeanModel));
-multiModelValuesAndResiduals <- data.frame(FittedValue = fitted.values(multiModel), Residual = residuals(multiModel));
-summary(meanModelValuesAndResiduals);
-summary(stdDevModelValuesAndResiduals);
-summary(lowestMeanModelValuesAndResiduals);
-summary(multiModelValuesAndResiduals);
+summary(data.frame(FittedValue = fitted.values(meanModel), Residual = residuals(meanModel)));
+summary(data.frame(FittedValue = fitted.values(stdDevModel), Residual = residuals(stdDevModel)));
+summary(data.frame(FittedValue = fitted.values(lowestMeanModel), Residual = residuals(lowestMeanModel)));
+summary(data.frame(FittedValue = fitted.values(multiModel), Residual = residuals(multiModel)));
 
 
-########################################
 ### Evaluate the accuracy of the models.
-########################################
 
 # Create a prediction with a 95% confidence interval.
 meanPredictedData_CI95 <- predict(meanModel, newdata = scoresToElos, interval = "confidence", level = 0.95);
@@ -318,8 +236,9 @@ stdDevPredictedData_CI95 <- predict(stdDevModel, newdata = scoresToElos, interva
 lowestMeanPredictedData_CI95 <- predict(lowestMeanModel, newdata = scoresToElos, interval = "confidence", level = 0.95);
 multiPredictedData_CI95 <- predict(multiModel, newdata = scoresToElos, interval = "confidence", level = 0.95);
 
-### Plotter used from DataModelingApproaches.R ###
+
 # Plot the 95% CI models.
+# NOTE: Plotter used from DataModelingApproaches.R
 # Average Delta Score Model
 OrdIn <- order(scoresToElos$AverageDeltaScore);
 par(mfrow = c(1,1));
